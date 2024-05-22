7bce37e454bd902cb66cca8eda6d668765388c14
ItzAnsh <anshbhasin3723@gmail.com>
2024-05-22 10:32:36 +0530

    Setup Commit 1

---
+++ BookWarden/BookWardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWardenApp.swift
+//  BookWarden
+//
+//  Created by Ansh Bhasin on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
